24ade28c47d266bb83b20b2162dedbb3f2115360
Camilo <camilo@ninjas.cl>
2020-01-14 10:47:09 -0300

    rm unused scripts

--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@
 ./scripts/rm-pw.sh
 ./scripts/download-pw.sh
 ./scripts/docker-compose-up.sh
-./scripts/copy-info.sh
 
 echo "Ready. Open http://localhost"
--- scripts/copy-info.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-echo "Copying phpinfo.php"
-cp ./scripts/phpinfo.php ./pw
